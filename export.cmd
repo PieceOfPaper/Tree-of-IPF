8e5d0b12563f757db4b2553060f260baec969cbf
The Paper <the.paper@live.co.kr>
2018-06-06 04:28:15 +0900

    addon.ipf

--- export.cmd
+++ export.cmd
@@ -5,6 +5,8 @@ if not exist data mkdir data
 if not exist patch mkdir patch
 cd ..
 
+if not exist %2 mkdir %2
+
 set TosPath=%1
 set RootPath=%cd%
 set TempPath=%cd%\Temp
@@ -16,7 +18,7 @@ set ExportPath=%cd%\%2
 cd %RootPath%
 pause
 for /f %%a in (ipflist_data.txt) do (
-	echo F|xcopy /y "%TosPath%\%%a" "%TempPath%\data\%%a"
+	xcopy /y "%TosPath%\%%a" "%TempPath%\data\%%a"
 	.\ipf_unpack.exe %TempPath%\data\%%a decrypt
 	.\ipf_unpack.exe %TempPath%\data\%%a extract xml lua dds xac png jpg tga imctree effect skn xsd xsm xsmtime wmove bin fx fxdb ttf export lma xpm fdp fev h txt lst mp3
 	
