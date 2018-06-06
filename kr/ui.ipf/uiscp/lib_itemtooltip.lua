61afd3b7200bcf7aae3bc8f89d17444853b60bd7
DESKTOP-DG0IVJ7\thepa <the.paper@live.co.kr>
2018-06-07 04:44:09 +0900

    Patch 142508_001001.ipf

--- kr/ui.ipf/uiscp/lib_itemtooltip.lua
+++ kr/ui.ipf/uiscp/lib_itemtooltip.lua
@@ -26,7 +26,7 @@ end
 function IS_NEED_DRAW_GEM_TOOLTIP(invitem)
 
 	local cnt = 0
-	for i=0, invitem.MaxSocket-1 do
+	for i=0, invitem.MaxSocket_COUNT-1 do
 		if invitem['Socket_' .. i] > 0 then
 			cnt = cnt + 1;
 		end
@@ -217,7 +217,6 @@ function ADD_ITEM_SOCKET_PROP(GroupCtrl, invitem, socket, gem, gemExp, gemLv, yP
 	end
 
 	local cnt = GroupCtrl:GetChildCount();
-	
 	local ControlSetObj			= GroupCtrl:CreateControlSet('tooltip_item_prop_socket', "ITEM_PROP_" .. cnt , 0, yPos);
 	local ControlSetCtrl		= tolua.cast(ControlSetObj, 'ui::CControlSet');
 
