e69dca08a83bcd24366dc5c1f07ff8327f397296
ThePaper <the.paper@live.co.kr>
2018-07-18 00:07:25 +0900

    Patch 210166_001001.ipf

--- export_patch.cmd
+++ export_patch.cmd
@@ -7,8 +7,6 @@ cd ..
 if not exist %2 mkdir %2
 
 
-
-
 set TosPath=%1
 set RootPath=%cd%
 set TempPath=%cd%\Temp
@@ -17,24 +15,39 @@ set ExportPath=%cd%\%2
 
 
 
+::===== ipf file Copy
+xcopy /y /s "%TosPath%\patch\*.ipf" "%TempPath%\patch\*.ipf" /EXCLUDE:%RootPath%\ipflist_patched.txt
+cd %TempPath%\patch
+dir /od /b > %RootPath%\ipflist_patch.txt
 
+
+cd %RootPath%
 for /f %%a in (ipflist_patch.txt) do (
 	cd %RootPath%
-	echo F|xcopy /y "%TosPath%\patch\%%a" "%TempPath%\patch\%%a"
+	
+	::===== Decrypt & Extract
 	%RootPath%\ipf_unpack.exe %TempPath%\patch\%%a decrypt
 	%RootPath%\ipf_unpack.exe %TempPath%\patch\%%a extract ies xml lua dds xac png jpg tga imctree effect skn xsd xsm xsmtime wmove bin fx fxdb ttf export lma xpm fdp fev h txt lst mp3 pathengine
 	
+	::===== Copy to Export Path
 	xcopy /y /s "%ExtractPath%" "%ExportPath%"
 	
+	::===== Delete Temp file
 	cd %RootPath%
-	del /s /q Temp
+	::del /s /q Temp
 	del /s /q extract
 	
+	::===== Commit & Push
 	cd %ExportPath%
 	git add --all
 	git commit -m "Patch %%a"
 	git push
 )
+
+
+::===== Save Pached list
+cd %TosPath%\patch
+dir /od /b > %RootPath%\ipflist_patched.txt
 
 
 ::====== temp data clear
