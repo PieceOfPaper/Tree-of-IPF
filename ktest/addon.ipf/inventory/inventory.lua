09d39095e911000fbcd9751ae936831f9a7fc2d7
ThePaper <the.paper@live.co.kr>
2018-07-21 04:37:14 +0900

    ktest Patch 154928_001001.ipf

--- ktest/addon.ipf/inventory/inventory.lua
+++ ktest/addon.ipf/inventory/inventory.lua
@@ -1368,12 +1368,15 @@ end
 
 function TRY_TO_USE_WARP_ITEM(invitem, itemobj)
 	local pc = GetMyPCObject();
-	if pc == nil or IsPVPServer(pc) == 1 or IsBuffApplied(pc, 'Event_Penalty') == 'YES' then
+	if pc == nil or IsPVPServer(pc) == 1 then
 		ui.SysMsg(ScpArgMsg("CannotUseThieInThisMap"));
 		return 0;
 	end
-
-
+	
+	if IsBuffApplied(pc, 'Event_Penalty') == 'YES' and (itemobj.ClassID == 640022 or itemobj.ClassID == 640022 or itemobj.ClassID == 640079 or itemobj.ClassID == 490006 or itemobj.ClassID == 490110)then
+	  ui.SysMsg(ScpArgMsg("CannotUseThieInThisMap"));
+		return 0;
+	end
 	-- 워프 주문서 예외처리. 실제 워프가 이루어질때 아이템이 소비되도록.
 	local warpscrolllistcls = GetClass("warpscrolllist", itemobj.ClassName);
 	if warpscrolllistcls ~= nil then
