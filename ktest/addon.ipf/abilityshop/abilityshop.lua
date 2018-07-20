d58d5bfca0a711ad0e252b6d29eeab506bb5e0fd
ThePaper <the.paper@live.co.kr>
2018-07-21 04:39:21 +0900

    ktest Patch 156109_001001.ipf

--- ktest/addon.ipf/abilityshop/abilityshop.lua
+++ ktest/addon.ipf/abilityshop/abilityshop.lua
@@ -615,10 +615,6 @@ function GET_ABILITY_PRICE(price, groupClass, abilClass, abilLv)
 --	else
 --	    price = price - (price * 0.2)
 	end
-	
-  if (GetServerNation() == "KOR" and GetServerGroupID() == 9001) then --큐폴 서버
-    price = price - (price * 0.8)
-  end
   
 	price = math.floor(price);
 	
