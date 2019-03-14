eaa5f2c24b28a1c7d9e3645b9ad9b89356ddde52
ThePaper <the.paper@live.co.kr>
2019-03-15 06:06:16 +0900

    ktest Patch All .3dworld files

--- export.cmd
+++ export.cmd
@@ -23,7 +23,7 @@ for /f %%a in (ipflist_data.txt) do (
 	cd %RootPath%
 	echo F|xcopy /y %TosPath%\data\%%a %TempPath%\data\%%a
 	%ToolsPath%\ipf_unpack\ipf_unpack.exe %TempPath%\data\%%a decrypt
-	%ToolsPath%\ipf_unpack\ipf_unpack.exe %TempPath%\data\%%a extract ies xml lua dds xac png jpg tga imctree effect skn xsd xsm xsmtime wmove bin fx fxdb ttf export lma xpm fdp fev h txt lst mp3
+	%ToolsPath%\ipf_unpack\ipf_unpack.exe %TempPath%\data\%%a extract ies xml lua dds xac png jpg tga imctree effect skn xsd xsm xsmtime wmove bin fx fxdb ttf export lma xpm fdp fev h txt lst mp3 pathengine atlas skel json 3dworld
 	
 	xcopy /y /s %ExtractPath% %ExportPath%
 	
