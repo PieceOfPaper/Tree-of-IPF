4a35e837e183cd59a4c928d51301724b9520635d
ThePaper <the.paper@live.co.kr>
2019-04-13 04:23:39 +0900

    global Patch 195875_001001.ipf

--- global/global.ipf/script/event/SCR_STEAM_EVENT_NOTICE_BOARD.lua
+++ global/global.ipf/script/event/SCR_STEAM_EVENT_NOTICE_BOARD.lua
@@ -1,58 +1,24 @@
 function SCR_STEAM_TREASURE_EVENT_DIALOG(self,pc)
+    local serverID = GetServerGroupID()
+    local select = 0;
 
-    local year, month, day, hour, min = GetAccountCreateTime(pc)
-    local aObj = GetAccountObj(pc)
-    local sObj = GetSessionObject(pc, 'ssn_klapeda')
-    local select = ShowSelDlg(pc, 0, 'EV_DAILYBOX_SEL', ScpArgMsg("Steam_VerUP_Select01"), ScpArgMsg("EVENT_SELECT_BOSSLV_SEL5"), ScpArgMsg("EVENT_STEAM_BEGINNER_SEL1"), ScpArgMsg("EVENT_STEAM_RETURN_SEL1"), ScpArgMsg("EVENT_STEAM_SETTLE_SEL1"), ScpArgMsg("Cancel")) 
-    local TeamLevel = GetTeamLevel(pc);
-
-    if TeamLevel == 1 then
-        local tx = TxBegin(pc)
-    	TxSetIESProp(tx, aObj, 'EV171114_STEAM_NRU_JOIN_CHECK', 1);
-    	local ret = TxCommit(tx)
+    if IsBuffApplied(pc, 'Event_Steam_Secret_Market') == 'YES' then
+        RemoveBuff(pc, 'Event_Steam_Secret_Market')
     end
-    
-    if select == 1 then
-        if aObj.EVENT_WHITE_R1 ~= 171212 then -- reset
-            local tx = TxBegin(pc)
-            TxSetIESProp(tx, aObj, 'EVENT_WHITE_R1', 171212)
-            TxSetIESProp(tx, aObj, 'EVENT_WHITE_R2', 0)
-            local ret = TxCommit(tx)
-        end
 
-        if sObj.EVENT_VALUE_SOBJ03 ~= 171212 and (pc.Lv <= 350 and pc.Lv >= 50) then
-            local nextLv = 0
-	        local nextlv_group = {330, 280, 235, 185, 135, 85, 45, 1}
-	        for i = 1, table.getn(nextlv_group) do
-        	    if pc.Lv >= nextlv_group[i] then
-        	        nextLv = i + pc.Lv
-        	        break
-        	    end
-        	end
-            ShowOkDlg(pc,'NPC_EVENT_VERUP_DLG1', 1)
-            local tx = TxBegin(pc)
-            TxAddIESProp(tx, sObj, 'EVENT_VALUE_SOBJ03', 171212);
-            TxSetIESProp(tx, sObj, 'EVENT_VALUE_SOBJ02', 0)
-            TxSetIESProp(tx, sObj, 'EVENT_VALUE_SOBJ01', nextLv)
-            TxGiveItem(tx, 'LevelUp_Reward_EV', 10, 'Event_VerUP_Box');
-            local ret = TxCommit(tx)
-        elseif pc.Lv == 360 and aObj.EVENT_WHITE_R2 == 0 then
-            local tx = TxBegin(pc)
-            TxSetIESProp(tx, aObj, 'EVENT_WHITE_R2', 171212)
-            TxGiveItem(tx, 'Premium_RankReset_60d', 1, 'Event_VerUP_Box2');
-            local ret = TxCommit(tx)
-        elseif sObj.EVENT_VALUE_SOBJ03 > 0 then
-            ShowOkDlg(pc,'NPC_EVENT_VERUP_DLG2', 1)
-        else
-            SendAddOnMsg(pc, 'NOTICE_Dm_!', ScpArgMsg("ICantLikeMe"), 5)
-        end
-    elseif select == 2 then
-        SCR_BLUEORB_MONLVUP_DIALOG(self,pc)
+    if serverID == 1001 or serverID == 1004 then
+        select = ShowSelDlg(pc, 0, 'EV_DAILYBOX_SEL', ScpArgMsg("Cancel"), ScpArgMsg("EVENT_COOPERATION_DLG4"), ScpArgMsg("EVENT_STEAM_2018REWARD_DLG1"), ScpArgMsg("EVENT_STEAM_2018REWARD_DLG2"), ScpArgMsg("EVENT_COOPERATION_DLG3"))
+    else
+        select = ShowSelDlg(pc, 0, 'EV_DAILYBOX_SEL', ScpArgMsg("Cancel"), ScpArgMsg("EVENT_COOPERATION_DLG4"), ScpArgMsg("EVENT_STEAM_2018REWARD_DLG1"), ScpArgMsg("EVENT_STEAM_2018REWARD_DLG2"))
+    end
+
+    if select == 2 then
+        SCR_GUILD_ATTENDANCE_DIALOG(self, pc)
     elseif select == 3 then
-        SCR_STEAM_BEGINNER_EVENT_DIALOG(self,pc)
+        SCR_EV2018_REWARD_GUIDE_DIALOG(self, pc)
     elseif select == 4 then
-        SCR_STEAM_RETURN_EVENT_DIALOG(self,pc)
+        SCR_EV2018_REWARD_DAYDAY_DIALOG(self, pc)
     elseif select == 5 then
-        SCR_STEAM_SETTLE_EVENT_DIALOG(self,pc)
+        SCR_EVENT_COOPERRATION_DIALOG(self, pc)
     end
 end
