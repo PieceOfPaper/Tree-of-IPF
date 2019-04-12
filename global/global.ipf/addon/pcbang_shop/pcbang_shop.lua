6442465382db6914741918b8a226ab64776b4b79
ThePaper <the.paper@live.co.kr>
2019-04-13 02:30:03 +0900

    global Patch 223626_001001.ipf

--- global/global.ipf/addon/pcbang_shop/pcbang_shop.lua
+++ global/global.ipf/addon/pcbang_shop/pcbang_shop.lua
@@ -18,3 +18,39 @@ function ON_PCBANG_SHOP_OPEN(frame)
         ON_PCBANG_SHOP_TAB_GUIDE(frame);
     end
 end
+
+function ON_UPDATE_PCBANG_SHOP_POINT(frame)
+    local point_text = GET_CHILD_RECURSIVELY(frame, "point_text");
+    local point_gauge = GET_CHILD_RECURSIVELY(frame, "point_gauge");
+    local today_point_text = GET_CHILD_RECURSIVELY(frame, "today_point_text");
+    local total_time_text = GET_CHILD_RECURSIVELY(frame, "total_time_text");
+    local point_timer_text = GET_CHILD_RECURSIVELY(frame, "point_timer_text");
+    
+    local pcBangPoint = session.pcBang.GetPCBangPoint();
+    point_text:SetTextByKey("value", pcBangPoint);
+    point_gauge:SetPoint(pcBangPoint, PCBANG_POINT_MAX_VALUE);
+    
+    local todayPoint = session.pcBang.GetPCBangTodayPoint();
+    today_point_text:SetTextByKey("value", todayPoint);
+    
+    local resetTime = session.pcBang.GetQuarterResetTime();
+    local now  = geTime.GetServerSystemTime();
+    local dif = imcTime.GetDifSec(resetTime, now);
+    SET_PCBANG_SHOP_RESET_TIME_TEXT(point_timer_text, dif)
+    SET_PCBANG_SHOP_TOTAL_TIME_TEXT(total_time_text, session.pcBang.GetTime("Total"))
+    
+	--point_timer_text:RunUpdateScript("UPDATE_PCBANG_SHOP_POINT_TIMER_TEXT", 60);
+	total_time_text:RunUpdateScript("UPDATE_PCBANG_SHOP_TOTAL_TIME_TEXT", 60);
+end
+
+function SET_PCBANG_SHOP_RESET_TIME_TEXT(point_timer_text, remainSec)
+    local day = math.floor(remainSec / 86400);
+    local remainder  = remainSec % 86400;
+    local hour = math.floor(remainder  / 3600);
+    remainder  = remainder  % 3600;
+    local min = math.floor(remainder  / 60);
+
+    -- point_timer_text:SetTextByKey("day", day);
+    -- point_timer_text:SetTextByKey("hour", hour);
+    -- point_timer_text:SetTextByKey("min", min);
+end
--- global/ui.ipf/uiscp/dummypc_ui.lua
+++ global/ui.ipf/uiscp/dummypc_ui.lua
@@ -1,8 +1,7 @@
 -- dummypc_ui.lua
 
 
-function POPUP_DUMMY(handle, targetInfo)
-
+function POPUP_DUMMY(handle, targetInfo)    
 	local context = ui.CreateContextMenu("DPC_CONTEXT", targetInfo.name, 0, 0, 100, 100);
 	
 	local ownerHandle = info.GetOwner(handle);
@@ -12,7 +11,7 @@ function POPUP_DUMMY(handle, targetInfo)
 	ui.AddContextMenuItem(context, ScpArgMsg("Auto_SalPyeoBoKi"), strscp);
 
 
---  ∏≈¿‘¿«∑⁄ªÛ¡°»ƒ «€∆€ ∞ÌøÎ ∏ﬁΩ√¡ˆ ¡¶∞≈	
+--  Îß§ÏûÖÏùòÎ¢∞ÏÉÅÏ†êÌõÑ ÌïºÌçº Í≥†Ïö© Î©îÏãúÏßÄ Ï†úÍ±∞	
 --	if ownerHandle == 0 then
 --		strscp = string.format("DUMMYPC_HIRE(%d)", handle);
 --		ui.AddContextMenuItem(context, ScpArgMsg("Auto_yongByeongKoyong"), strscp);
@@ -22,11 +21,12 @@ function POPUP_DUMMY(handle, targetInfo)
 --		ui.AddContextMenuItem(context, ScpArgMsg("Auto_yongByeongHaeKo"), strscp);
 --	end
 
-	if true == session.IsGM() then
+	if 1 == session.IsGM() then        
 		strscp = string.format("debug.TestE(%d)", handle);
 		ui.AddContextMenuItem(context, ScpArgMsg("Auto_{@st42b}NodeBoKi{/}"), strscp);
 		strscp = string.format("ui.Chat(\"//killmon %d\")", handle);
-		ui.AddContextMenuItem(context, ScpArgMsg("Auto_JeKeo"), strscp);
+		ui.AddContextMenuItem(context, ScpArgMsg("Auto_JeKeo"), strscp)
+        ui.AddContextMenuItem(context, ScpArgMsg("GM_Order_Kick"), string.format("REQUEST_ORDER_DUMMY_KICK(\"%s\")", handle))
 	end
 
 	if session.world.IsIntegrateServer() == false then
@@ -34,10 +34,12 @@ function POPUP_DUMMY(handle, targetInfo)
 		ui.AddContextMenuItem(context, ScpArgMsg("VisitBarrack"), strscp);
 	end
 
-	ui.AddContextMenuItem(context, ScpArgMsg("Auto_DatKi"),  "");		
-	
-	ui.OpenContextMenu(context);
+	ui.AddContextMenuItem(context, ScpArgMsg("Auto_DatKi"),  "")	
+	ui.OpenContextMenu(context)
+end
 
+function REQUEST_ORDER_DUMMY_KICK(handle)
+    packet.RequestGmOrderDummyKick(tonumber(handle))
 end
 
 function DUMMYPC_SHOWINFO(handle)
