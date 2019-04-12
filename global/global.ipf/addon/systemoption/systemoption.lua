3d352102b2ece4ee460007d2e22983f48735a9eb
ThePaper <the.paper@live.co.kr>
2019-04-12 23:01:09 +0900

    global Patch 151628_001001.ipf

---
+++ global/global.ipf/addon/systemoption/systemoption.lua
@@ -0,0 +1,49 @@
+function INIT_SCREEN_CONFIG(frame)
+
+	local getGroup = GET_CHILD(frame, "pipwin_low", "ui::CGroupBox")
+	local getPipwin_low = GET_CHILD(frame, "pipwin_low", "ui::CGroupBox")
+	local catelist = GET_CHILD(getPipwin_low, "resolutionList", "ui::CDropList");
+	catelist:ClearItems();
+
+	local curWidth = option.GetClientWidth();
+	local curHeight = option.GetClientHeight();
+	local selIndex = 0;
+
+	local cnt = option.GetDisplayModeCount();
+
+	for i = 0 , cnt - 1 do
+		local width = option.GetDisplayModeWidth(i);
+		local height = option.GetDisplayModeHeight(i);
+		local resString = string.format("{@st42b}%d * %d{/}", width, height);
+		catelist:AddItem(i, resString);
+
+		if curWidth == width and curHeight == height then
+			selIndex = i;
+		end
+	end
+
+	catelist:SelectItem(selIndex);
+	local scrMode = option.GetScreenMode();
+	local scrBtn = GET_CHILD_RECURSIVELY(frame,"scrtype_" .. scrMode,"ui::CRadioButton");
+	if scrBtn ~= nil then
+		scrBtn:Select();
+	end
+
+	local autoPerfMode = config.GetAutoAdjustLowLevel();
+	local autoPerfBtn = GET_CHILD_RECURSIVELY(frame,"perftype_" .. autoPerfMode);
+	if autoPerfBtn ~= nil then
+		autoPerfBtn:Select();
+	end
+	
+	local chkOptimization = GET_CHILD_RECURSIVELY(frame, "check_optimization", "ui::CCheckBox");
+	if nil ~= chkOptimization then
+		chkOptimization:SetCheck(imcperfOnOff.IsEnableOptimization());
+	end;
+
+	local syncMode = option.IsEnableVSync()
+	local syncBtn = GET_CHILD_RECURSIVELY(frame,"vsync_" .. syncMode,"ui::CRadioButton");
+	if syncBtn ~= nil then
+		syncBtn:Select()
+	end
+
+end
--- global/global.ipf/script/event/SCR_STEAM_EVENT_NOTICE_BOARD.lua
+++ global/global.ipf/script/event/SCR_STEAM_EVENT_NOTICE_BOARD.lua
@@ -1,37 +1,37 @@
 function SCR_STEAM_TREASURE_EVENT_DIALOG(self,pc)
-    if pc.Lv < 100 then
-        ShowOkDlg(pc, 'EV_PRISON_DESC2', 1)
-        return
-    end
-    local aObj = GetAccountObj(pc);
-    
-    EVENT_PROPERTY_RESET(pc, aObj, sObj)
-    
-    if pc.MHP - pc.HP > 0 then
-        AddHP(pc, pc.MHP - pc.HP);
-    end
-    
-    local select = ShowSelDlg(pc, 0, 'EV_DAILYBOX_SEL', ScpArgMsg("Prison_Select1"), ScpArgMsg("Prison_Select3"), ScpArgMsg("Prison_Select2", "COUNT", aObj.PlayTimeEventRewardCount), ScpArgMsg("Cancel"))
-    
-    if select == 1 then
-        AUTOMATCH_INDUN_DIALOG(pc, nil, 'Indun_d_prison_62_1_event')
-    elseif select == 2 then
-        AUTOMATCH_INDUN_DIALOG(pc, nil, 'Indun_d_prison_event_easy')
-    elseif select == 3 then
-        if aObj.PlayTimeEventRewardCount >= 10 and aObj.Event_HiddenReward == 0 then
-            local tx = TxBegin(pc)
-            TxAddIESProp(tx, aObj, 'Event_HiddenReward', 1);
-            TxGiveItem(tx, 'Premium_Enchantchip14', 3, 'Prison_Event');
-        	local ret = TxCommit(tx)
-        elseif aObj.PlayTimeEventRewardCount >= 20 and aObj.Event_HiddenReward == 1 then
-            local tx = TxBegin(pc)
-            TxAddIESProp(tx, aObj, 'Event_HiddenReward', 1);
-            TxGiveItem(tx, 'Hat_628290', 1, 'Prison_Event');
-        	local ret = TxCommit(tx)
-        else
-            ShowOkDlg(pc, 'EV_PRISON_DESC1')
-    	end
-    end
+--    if pc.Lv < 100 then
+--        ShowOkDlg(pc, 'EV_PRISON_DESC2', 1)
+--        return
+--    end
+--    local aObj = GetAccountObj(pc);
+--    
+--    EVENT_PROPERTY_RESET(pc, aObj, sObj)
+--    
+--    if pc.MHP - pc.HP > 0 then
+--        AddHP(pc, pc.MHP - pc.HP);
+--    end
+--    
+--    local select = ShowSelDlg(pc, 0, 'EV_DAILYBOX_SEL', ScpArgMsg("Prison_Select1"), ScpArgMsg("Prison_Select3"), ScpArgMsg("Prison_Select2", "COUNT", aObj.PlayTimeEventRewardCount), ScpArgMsg("Cancel"))
+--    
+--    if select == 1 then
+--        AUTOMATCH_INDUN_DIALOG(pc, nil, 'Indun_d_prison_62_1_event')
+--    elseif select == 2 then
+--        AUTOMATCH_INDUN_DIALOG(pc, nil, 'Indun_d_prison_event_easy')
+--    elseif select == 3 then
+--        if aObj.PlayTimeEventRewardCount >= 10 and aObj.Event_HiddenReward == 0 then
+--            local tx = TxBegin(pc)
+--            TxAddIESProp(tx, aObj, 'Event_HiddenReward', 1);
+--            TxGiveItem(tx, 'Premium_Enchantchip14', 3, 'Prison_Event');
+--        	local ret = TxCommit(tx)
+--        elseif aObj.PlayTimeEventRewardCount >= 20 and aObj.Event_HiddenReward == 1 then
+--            local tx = TxBegin(pc)
+--            TxAddIESProp(tx, aObj, 'Event_HiddenReward', 1);
+--            TxGiveItem(tx, 'Hat_628290', 1, 'Prison_Event');
+--        	local ret = TxCommit(tx)
+--        else
+--            ShowOkDlg(pc, 'EV_PRISON_DESC1')
+--    	end
+--    end
 end
 
 function EVENT_PROPERTY_RESET(pc, aObj, sObj)
