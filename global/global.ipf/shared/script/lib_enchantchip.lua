275a420282e13237af5841be456c99d9070150f8
ThePaper <the.paper@live.co.kr>
2019-04-13 05:31:58 +0900

    global Patch 244684_001001.ipf

--- global/global.ipf/shared/script/lib_enchantchip.lua
+++ global/global.ipf/shared/script/lib_enchantchip.lua
@@ -1,5 +1,5 @@
 function IS_ENCHANT_ITEM(item)
-	if item.ClassName == "Premium_Enchantchip" or item.ClassName == "Premium_Enchantchip14" or item.ClassName == "Premium_Enchantchip_CT" or item.ClassName == "Premium_Enchantchip14_NoStack" then
+	if item.ClassName == "Premium_Enchantchip" or item.ClassName == "Premium_Enchantchip14" or item.ClassName == "Premium_Enchantchip_CT" or item.ClassName == "Premium_Enchantchip14_NoStack" or item.ClassName == "TeamBat_Enchantchip" or item.ClassName == "Adventure_Enchantchip" or item.ClassName == "Premium_Enchantchip14_Team"or item.ClassName == "STEAM_MASTER_Premium_Enchantchip" then
 		return 1;
 	end
 
