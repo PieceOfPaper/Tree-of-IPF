2862f09fb70f79b357efa564830205ebcd59fd0d
ThePaper <the.paper@live.co.kr>
2020-07-26 15:39:51 +0900

    kr Patch 285752_001001.ipf

--- kr/addon.ipf/minimized_housing_promote_board/minimized_housing_promote_board.lua
+++ kr/addon.ipf/minimized_housing_promote_board/minimized_housing_promote_board.lua
@@ -3,33 +3,5 @@ function MINIMIZED_HOUSING_PROMOTE_BOARD_ON_INIT(addon, frame)
 end
 
 function MINIMIZED_HOUDING_PROMOTE_BOARD_BUTTON_OPEN_CHECK(frame)
-	local mapprop = session.GetCurrentMapProp();
-    local mapCls = GetClassByType("Map", mapprop.type);	
-    
-    if IS_TOWN_MAP(mapCls) == false then
-        frame:ShowWindow(0);
-    else
-    	frame:ShowWindow(1);
-    end
-end
-
-function REQUEST_HOUSING_PROMOTE_BOARD_OPEN(frame)
-    local boardframe = ui.GetFrame("housing_promote_board");
-	if boardframe:IsVisible() == 1 then
-		return;
-    end
-    
-    HOUSING_PROMOTE_BOARD_OPEN();
-
-    local openBtn = GET_CHILD(frame, "openBtn");
-    openBtn:SetEnable(0);
-
-    ReserveScript("RESET_HOUSING_PROMOTE_BOARD_OPEN()", 5);
-end
-
-function RESET_HOUSING_PROMOTE_BOARD_OPEN()
-    local frame = ui.GetFrame("minimized_housing_promote_board");
-
-    local openBtn = GET_CHILD(frame, "openBtn");
-    openBtn:SetEnable(1);
+    frame:ShowWindow(0);
 end
